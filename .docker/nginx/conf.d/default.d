22a0b475f284a94a8ae4be01c2f287d607866a41
pv <pv@acRennes.fr>
2026-09-21 11:56:22 -0400

    Ajout generation des certificat/ Modification nom image de php

--- .docker/nginx/conf.d/default.d
+++ .docker/nginx/conf.d/default.d
@@ -2,7 +2,7 @@ server {
     listen 80 default_server;
     root   /var/www/php;
     index  index.html;
-    server_name 192.168.1.30;
+    server_name 192.168.1.69;
 
     # Redirige toutes les requêtes HTTP vers HTTPS
     return 301 https://$host$request_uri;
@@ -12,14 +12,14 @@ server {
 server {
     listen 443 ssl http2;  # Activation du support HTTP/2 pour de meilleures performances
     listen [::]:443 ssl http2;
-    server_name 192.168.1.30;
+    server_name 192.168.1.69;
 
     root /var/www/php;
     index index.html index.php;
 
     # Définition des certificats SSL 
-    ssl_certificate /etc/nginx/ssl/serveur_PV_30.crt;
-    ssl_certificate_key /etc/nginx/ssl/serveur_PV_30.key;
+    ssl_certificate /etc/nginx/ssl/server_69.crt;
+    ssl_certificate_key /etc/nginx/ssl/server_69.key;
 
     # Configuration de PHP via FastCGI
     location ~* \.php$ {
